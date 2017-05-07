5e34d489c920672c13a93319af1555ad607f7066
Oscar Romeo Ramos Molina <RomeoRamos@MacBook-Pro-de-Oscar-2.local>
2017-05-07 13:10:42 -0500

    New user model defined, databases set, new apss created

--- Musicbox/Musicbox/urls.py
+++ Musicbox/Musicbox/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^',include("Modules.Landing.urls", namespace="landing")),
+    url(r'^',include("modules.Landing.urls", namespace="landing")),
 ]
