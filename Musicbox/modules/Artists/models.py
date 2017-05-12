29acb80443ba9e2593dbfbd23a714082fc797a94
Oscar Romeo Ramos Molina <RomeoRamos@MacBook-Pro-de-Oscar-2.local>
2017-05-11 23:03:30 -0500

    Avanzo con modelos, falta resolver song_file

--- Musicbox/modules/Artists/models.py
+++ Musicbox/modules/Artists/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.conf import settings
 
 class Artist(models.Model):
     id = models.AutoField(primary_key=True)
@@ -7,6 +8,7 @@ class Artist(models.Model):
     nationality = models.CharField(max_length=50)
     bio = models.TextField()
     category = category = models.CharField(max_length=100,choices=settings.CATEGORIES)
+    user_creator =models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
--- Musicbox/modules/Songs/models.py
+++ Musicbox/modules/Songs/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from modules.Artists.models import Artist
+from modules.Albums.models import Album
 from django.conf import settings
 
 # Create your models here.
@@ -9,9 +10,10 @@ class Song(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     artist = models.ForeignKey(Artist,on_delete=models.CASCADE,related_name='artist_song')
+    album = models.ForeignKey(Album,on_delete=models.CASCADE,related_name='album_song')
     publication_date = models.DateField(null=True)
     cover = models.URLField(blank=True,null=True)
-    rating = models.IntegerField(max_digits=1)
+    rating = models.IntegerField()
     category = models.CharField(max_length=100,choices=settings.CATEGORIES)
     song_file = FileField(uploadto="filename")
     user_creator =models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.SET_NULL)
